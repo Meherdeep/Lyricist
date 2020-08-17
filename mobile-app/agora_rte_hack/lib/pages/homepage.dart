a037cc2884799fcb550ec924a9d0815a3738d418
meherdeept <meherdeept@gmail.com>
2020-08-17 11:01:27 +0530

    app: audio player

--- mobile-app/agora_rte_hack/lib/pages/homepage.dart
+++ mobile-app/agora_rte_hack/lib/pages/homepage.dart
@@ -1,5 +1,7 @@
 import 'package:agora_rte_hack/utils/helper.dart';
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
+import 'package:audioplayers/audio_cache.dart';
 
 class MyHomePage extends StatefulWidget {
   @override
@@ -9,7 +11,12 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
 
   bool isLiked = false;
+
+  bool isPaused = false;
   
+
+  AudioPlayer audioPlayer;
+
   PageController _controller = PageController(
     initialPage: 0,
   );
@@ -18,12 +25,46 @@ class _MyHomePageState extends State<MyHomePage> {
   void dispose() {
     _controller.dispose();
     super.dispose();
+    audioPlayer = null;
   }
   
   List<String> bgLocation = ['assets/kodaline.jpg','assets/Bleached.jpg','assets/Interrupters.jpg','assets/linkin_park.jpg','assets/Lumineers.jpg','assets/Rob-Thomas.jpg'];
   
   List<String> artistName = ['Ralph','Connie','Niall','Troy','Herman','Douglas'];
 
+  List<String> albumCovers = ['assets/1.png','assets/2.png','assets/3.png'];
+
+  List<String> songName = ['BOP','Panini','Heavenly'];
+
+  playAudioFromLocalStorage(path) async{
+    audioPlayer = await AudioCache().loop(path);
+  }
+
+  pauseAudio() async{
+    int response = await audioPlayer.pause();
+    print(response);
+  }
+
+  resumeAudio() async{
+    int response = await audioPlayer.resume();
+    print(response);
+  }
+
+  songDuration() async{
+    int duration = await audioPlayer.getDuration();
+    print(duration);
+  }
+
+
+  Widget slider(){
+    return Slider(
+      activeColor: Color(0xFFCD2F51),
+      inactiveColor: Colors.black,
+      value: null, 
+      onChanged: null
+    );
+  }
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -117,22 +158,74 @@ class _MyHomePageState extends State<MyHomePage> {
                 ),
               ),
               Align(
-                alignment: Alignment.bottomCenter,
+                alignment: Alignment(-0.6, 0.9),
                 child: Container(
-                  width: MediaQuery.of(context).size.width*0.9,
+                  height: MediaQuery.of(context).size.height*0.1,
                   child: ListView.builder(
                     itemBuilder: (context,itemCount){
-                      return Container(
-                        decoration: BoxDecoration(
-                          color: Color(0xFFCD2F51),
-                          borderRadius: BorderRadius.circular(40),
+                      return Padding(
+                        padding: const EdgeInsets.only(left: 15.0),
+                        child: Row(
+                          children: <Widget>[
+                            Container(
+                              width: MediaQuery.of(context).size.width*0.6,
+                              height: MediaQuery.of(context).size.height*0.1,
+                              decoration: BoxDecoration(
+                                color: Colors.black54,
+                                borderRadius: BorderRadius.circular(40)
+                              ),
+                              child: Row(
+                                children: <Widget>[
+                                  Stack(
+                                    children: <Widget>[
+                                      CircleAvatar(
+                                        backgroundImage: ExactAssetImage(albumCovers[itemCount]),
+                                        radius: 37,
+                                      ),
+                                      Center(
+                                        child: IconButton(
+                                          icon: isPaused? Icon(Icons.play_arrow, color: Colors.white, size: 30): Icon(Icons.pause, color: Colors.white, size: 30,), 
+                                          onPressed: (){
+                                            setState(() {
+                                              isPaused = !isPaused;
+                                            });
+                                            isPaused?pauseAudio():playAudioFromLocalStorage('sample.mp3');
+                                          }
+                                        ),
+                                      )
+                                    ],
+                                  ),
+                                  SizedBox(
+                                    width: 10,
+                                  ),
+                                  Column(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: <Widget>[
+                                      Text(
+                                        songName[itemCount], 
+                                        style: TextStyle(
+                                          color: Colors.white,
+                                          fontSize: 18
+                                        )
+                                      ),
+                                      Text(artistName[position], style: TextStyle(color:Colors.white60, fontSize: 11), textAlign: TextAlign.left,)
+                                    ],
+                                  )
+                                ],
+                              ),
+                            ),
+                            SizedBox(
+                              width: MediaQuery.of(context).size.width*0.08,
+                            )
+                          ],
                         ),
-                      ); 
+                      );
                     },
-                    itemCount: bgLocation.length,
+                    itemCount: 3,
+                    scrollDirection: Axis.horizontal,
                   ),
                 )
-              ),
+              )
             ],
           );
         },
