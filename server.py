4be636baaaa2c6b687bf9bed6ac4960eefc44719
Sandeep <rayanuthalas@gmail.com>
2020-08-20 11:20:56 +0530

    Pushed

--- server.py
+++ server.py
@@ -16,17 +16,23 @@ website_URL = "https://www.dcode.fr/music-sheet"
 
 def GetNodesScrapper(SOME_RANDOM_SHIT):
     brower.get(website_URL)
+    print("stage1")
     brower.find_element_by_id("convert_from_abc_to_sheet_notes_abc").send_keys(Keys.CONTROL + "a")
+    print("stage2")
     brower.find_element_by_id("convert_from_abc_to_sheet_notes_abc").send_keys(Keys.DELETE)
+    print("stage3")
     brower.find_element_by_id("convert_from_abc_to_sheet_notes_abc").send_keys(SOME_RANDOM_SHIT)
+    print("stage4")
     brower.find_element_by_xpath('//form[@id="convert_from_abc_to_sheet"]/button[@post="notes_abc"]').click()
+    print("stage5")
     time.sleep(2)
     value = brower.find_element_by_name("download").get_attribute("value")
+    print("stage6")
     fvalue = base64.b64decode(value).decode("utf-8")
     return fvalue.replace("\\", '')[1:][:1]
 
 
-@app.get("/getNodes")
+@app.post("/getNodes")
 async def GetHTML(req: Request):
     body = await req.json()
     return GetNodesScrapper(body['nodes'])
